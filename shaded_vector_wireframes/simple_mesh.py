25dcf624389084b33c0e1e7be1ca4e94d1eb798e
Adira Blumenthal <adiraeb@gmail.com>
2023-10-11 10:03:46 -0400

    add class notes

--- shaded_vector_wireframes/simple_mesh.py
+++ shaded_vector_wireframes/simple_mesh.py
@@ -4,7 +4,10 @@ import numpy as np
 step = cq.importers.importStep("../cadquerytests/blumenthal/C270_assembly.step")
 tolerance = [0.001, 0.031, 0.051, 0.071, 0.091, 0.1]
 angularTolerance = [0.07, 0.08, 0.09, 0.1]
-
+# smallest size that is still accurate
+# get a sense of what the parameters do to the size 
+# focus wheel - individual parts 
+# layer height for printing affects the angular tolerance -- how do the print settings affect tolerance/angular tolerance settings?
 for t in tolerance:
     for a in angularTolerance:
         # print(t, a)
